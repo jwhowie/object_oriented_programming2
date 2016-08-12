6e8e138ebf703e9d5f30aea448a885d83366d2de
John Howie <jkhowie63@gmail.com>
2016-08-12 11:58:05 -0400

    changed git respositories

---
+++ class_time/people.rb
@@ -0,0 +1,32 @@
+class Person
+  def initialize(name)
+    @name = name
+  end
+
+  def greeting
+    puts "Hi, my name is #{@name}"
+  end
+end
+
+class Student < Person
+  def learn
+    puts 'I get it!'
+  end
+end
+
+class Instructor < Person
+  def teach
+    puts 'Everything in Ruby is an Object'
+  end
+end
+
+chris = Instructor.new('Chris')
+chris.greeting
+
+cristina = Student.new('Cristina')
+cristina.greeting
+
+chris.teach
+cristina.learn
+# Cannot call teach on a student as the method is not defined in the student class hierarchy
+#cristing.teach
